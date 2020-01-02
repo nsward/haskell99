9d503a7342f00769a321d480b4113df99794d0dc
nsward <wardnick@protonmail.com>
2020-01-02 14:06:32 -0500

    Prob 2

--- lists.hs
+++ lists.hs
@@ -9,3 +9,13 @@ myLast :: [a] -> a
 myLast [] = error "empty list"
 myLast [x] = x
 myLast (_:xs) = myLast xs
+
+---- 2: Find the last but one element of a list.
+-- λ> myButLast [1,2,3,4]
+-- 3
+-- λ> myButLast ['a'..'z']
+-- 'y'
+myButLast :: [a] -> a
+myButLast x
+    | length x < 2 = error "not enough elements"
+    | otherwise = x !! (length x - 2)
