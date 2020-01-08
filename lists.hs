b6416f1a74c77e341f37415e1ded4ba44ea51b46
nsward <wardnick@protonmail.com>
2020-01-08 21:57:04 +0400

    Prob 10

--- lists.hs
+++ lists.hs
@@ -90,12 +90,22 @@ compress = foldr (\x xs -> if isDup x xs then xs else [x] ++ xs) []
 -- λ> pack ['a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e']
 -- ["aaaa","b","cc","aa","d","eeee"]
 -- first version:
-pack :: Eq a => [a] -> [[a]]
-pack [] = []
-pack (x:xs) = (x:takeWhile (== x) xs):pack (dropWhile (==x) xs)
-
--- better version:
 pack' :: Eq a => [a] -> [[a]]
 pack' [] = []
-pack' (x:xs) = let (match, rem) = span (== x) xs
+pack' (x:xs) = (x:takeWhile (== x) xs):pack (dropWhile (==x) xs)
+
+-- better version:
+pack :: Eq a => [a] -> [[a]]
+pack [] = []
+pack (x:xs) = let (match, rem) = span (== x) xs
     in (x:match):pack rem
+
+----- 10: Run-length encoding of a list. Use the result of problem P09 to implement the so-called run-length encoding data compression method. Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of the element E.
+-- * (encode '(a a a a b c c a a d e e e e))
+-- ((4 A) (1 B) (2 C) (2 A) (1 D)(4 E))
+--
+-- λ> encode "aaaabccaadeeee"
+-- [(4,'a'),(1,'b'),(2,'c'),(2,'a'),(1,'d'),(4,'e')]
+encode :: Eq a => [a] -> [(Int, a)]
+encode [] = []
+encode xs = map (\xs -> (length xs, head xs)) (pack xs)
