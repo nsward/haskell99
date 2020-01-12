e249407596ba065ac4ba2549e2960c9ccc221f45
nsward <wardnick@protonmail.com>
2020-01-12 08:27:04 +0400

    Prob 13

--- Lists_1.hs
+++ Lists_1.hs
@@ -29,3 +29,16 @@ decodeMod = concatMap decoder
     where
      decoder (Single a) = [a]
      decoder (Multiple n a) = replicate n a
+
+----- 13: Run-length encoding of a list (direct solution).
+-- Implement the so-called run-length encoding data compression method directly. I.e. don't explicitly create the sublists containing the duplicates, as in problem 9, but only count them. As in problem P11, simplify the result list by replacing the singleton lists (1 X) by X.
+-- λ> encodeDirect "aaaabccaadeeee"
+-- [Multiple 4 'a',Single 'b',Multiple 2 'c',
+--  Multiple 2 'a',Single 'd',Multiple 4 'e']
+encodeDirect :: Eq a => [a] -> [EncodedListItem a]
+encodeDirect [] = []
+encodeDirect (x:xs) = let matchLen = length (takeWhile (== x) xs)
+    in toEli (matchLen + 1) x : encodeDirect (drop matchLen xs)
+    where
+     toEli 1 a = Single a
+     toEli n a = Multiple n a
