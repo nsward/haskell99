9cd108fb320d6843cf3f5a5662b27ebd14d01b71
nsward <wardnick@protonmail.com>
2020-01-14 18:21:04 +0400

    Prob 18

--- Lists_1.hs
+++ Lists_1.hs
@@ -94,3 +94,16 @@ drop' n (x:xs) = drop' (n - 1) xs
 
 split' :: [a] -> Int -> ([a], [a])
 split' xs n = (take' n xs, drop' n xs)
+
+----- 18: Extract a slice from a list
+-- Given two indices, i and k, the slice is the list containing the elements between the i'th and k'th element of the original list (both limits included). Start counting the elements with 1.
+-- λ> slice ['a','b','c','d','e','f','g','h','i','k'] 3 7
+-- "cdefg"
+slice :: [a] -> Int -> Int -> [a]
+slice (x:xs) i k
+    | k >= i && i == 1 = x : slice xs 1 (k - 1)
+    | k > i && i > 1 = slice xs (i - 1) (k - 1)
+    | otherwise = []
+
+slice' :: [a] -> Int -> Int -> [a]
+slice' xs i k = take (k - i + 1) (drop (i - 1) xs)
