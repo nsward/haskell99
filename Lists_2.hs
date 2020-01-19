9ec1e600ed20f2c99b6985e1746611bb713ca6d2
nsward <wardnick@protonmail.com>
2020-01-19 08:08:06 +0400

    Prob 22

--- Lists_2.hs
+++ Lists_2.hs
@@ -9,3 +9,13 @@ import Data.List
 insertAt :: a -> [a] -> Int -> [a]
 insertAt y xs 1 = y:xs
 insertAt y (x:xs) n = x : insertAt y xs (n - 1)
+
+----- 22: Create a list containing all integers within a given range
+-- λ> range 4 9
+-- [4,5,6,7,8,9]
+range :: Int -> Int -> [Int]
+-- range a b = [a..b]
+range a b
+    | a > b = []
+    | otherwise = a : range (a + 1) b
+
